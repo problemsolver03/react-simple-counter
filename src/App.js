22a89c796f07b7bca8da3de8fb9692a3f25494a2
problemsolver03 <designangel2013@gmail.com>
2021-07-18 17:00:33 +0530

    logic for state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React,{useState} from 'react'
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
 
-const App = ()=>{
+const App = () => {
+  let [score, setScore] = useState(0);
 
-    let [score, setScore] = useState(0)
-    
-    return(
-        <div>
-             <h1> Hey this is my Counter Application </h1>
-             <h3>The value of score is {score}</h3>
-             <button onClick={()=>{setScore(score+1)}}> Increment </button>
-             <button onClick={()=>(score>0)? setScore(score-1) : "" }> Decrement </button>
-             <button onClick={()=>{setScore(0)}}> Reset </button>
-        </div>
-    )
-}
+  return (
+    <div className="container text-center mt-5">
+      <h1> Hey this is my Counter Application </h1>
+      <h3 className="mb-5">The value of score is {score}</h3>
+      <button
+        className="btn btn-primary mr-2"
+        onClick={() => {
+          if (score < 25) {
+            setScore(score + 1);
+          }
+        }}
+      >
+        + Increment
+      </button>{" "}
+      <button
+        className="btn btn-primary mr-2"
+        onClick={() => (score > 0 ? setScore(score - 1) : "")}
+      >
+        - Decrement
+      </button>{" "}
+      <button
+        className="btn btn-danger"
+        onClick={() => {
+          setScore(0);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
 
-export default App
-
-
-
-
-
-
+export default App;
